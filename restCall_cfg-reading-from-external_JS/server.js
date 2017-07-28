270ac3611bcb841a8605a8f2d75c35de7c9cf93f
Gurudasnov <svt.subscribe@gmail.com>
2017-07-28 11:13:19 +0530

    Delete server.js

--- restCall_cfg-reading-from-external_JS/server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello New York\n');
-}).listen(8088);
-console.log('Server running at http://localhost:8088/')
