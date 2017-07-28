21dfb3fdb9b54041e26a3b775fbf047333c98136
Gurudasnov <svt.subscribe@gmail.com>
2017-07-28 11:10:11 +0530

    Create readme.js

---
+++ restCall_cfg-reading-from-external_JS/readme.js
@@ -0,0 +1,3 @@
+You need to keep NodeJS setup ready.
+in this application we are calling REST service and reading URL from config.js.
+so you need to update Config.js accouring to rest URL Services.
